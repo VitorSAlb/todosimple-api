5b4551ff69f7e15eabf85f2a31714bf64de18322
VitorSAlb <vitor.s.albq@gmail.com>
2024-02-26 10:36:49 -0300

    Spring Boot Project

---
+++ src/test/java/com/vitorsalb/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorsalb.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
